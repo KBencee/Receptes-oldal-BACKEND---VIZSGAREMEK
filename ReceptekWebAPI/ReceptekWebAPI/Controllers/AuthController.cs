36e97c343f5cef2609bd148713032d8cb9f0dbf7
KBencee <kissbence225@gmail.com>
2026-01-14 09:51:07 +0100

    alap + login endpoint

---
+++ ReceptekWebAPI/ReceptekWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ReceptekWebAPI.Entities;
+using ReceptekWebAPI.Models;
+
+namespace ReceptekWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public static User user = new User();
+
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>()
+                .HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login(UserDto request)
+        {
+            if(user.Username != request.Username) 
+            {
+                return BadRequest("User not found.");           
+            }
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password)
+                == PasswordVerificationResult.Failed) 
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = "success";
+            return Ok(token);
+        }
+    }
+}
