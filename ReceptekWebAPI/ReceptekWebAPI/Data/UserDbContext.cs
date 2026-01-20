d0c41b6288d5cc66c42da95c8047cdc67e219dab
KBencee <kissbence225@gmail.com>
2026-01-20 10:15:46 +0100

    db létrehozva

---
+++ ReceptekWebAPI/ReceptekWebAPI/Data/UserDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReceptekWebAPI.Entities;
+
+namespace ReceptekWebAPI.Data
+{
+    public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
--- ReceptekWebAPI/ReceptekWebAPI/Program.cs
+++ ReceptekWebAPI/ReceptekWebAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using ReceptekWebAPI.Data;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<UserDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("UserDatabase"))
+);
 
 var app = builder.Build();
 
